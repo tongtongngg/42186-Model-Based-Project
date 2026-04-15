aaa358ca16c68740fdfda405cbf537644581ad21
maltheaugust <s214631@dtu.dk>
2026-04-15 15:17:54 +0200

    added dataset loading function

---
+++ src/data_utils/load_dataset.py
@@ -0,0 +1,17 @@
+import glob
+import os
+
+import kagglehub
+import pandas as pd
+
+def load_PL_dataset() -> pd.DataFrame:
+    path = kagglehub.dataset_download("sananmuzaffarov/epl-202526-player-stats-gw131")
+    csv_files = glob.glob(os.path.join(path, "*.csv"))
+    df = pd.read_csv(csv_files[0], encoding='latin-1')
+
+    return df
+
+if __name__ == "__main__":
+    df = load_PL_dataset()
+
+    print(df.head(5))
