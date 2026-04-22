1eb599d4d854c977e74de75ec0e44cce6830f935
Jesper <Jesper.c.aalose@live.dk>
2026-04-22 13:39:39 +0200

    defender

--- src/models/defender_model.py
+++ src/models/defender_model.py
@@ -1,1 +1,10 @@
+from src.data_utils import get_correlations_by_position, load_PL_dataset
 
+
+if __name__ == "__main__":
+
+    df = load_PL_dataset()
+
+    correlations = get_correlations_by_position(df, positions=['D'], cutoff=0.6)
+
+    print(list(correlations['M'].items()))
