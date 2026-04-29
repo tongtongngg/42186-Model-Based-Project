46078e2f79a4fd0ec36b77e882ec458d685bc7f8
maltheaugust <s214631@dtu.dk>
2026-04-29 15:55:45 +0200

    finished goalkeeper model

--- src/models/goalkeeper_model.py
+++ src/models/goalkeeper_model.py
@@ -16,45 +16,53 @@ def goalkeeper_model(saves=None, accuratePasses=None, ballRecovery=None,
     Inputs are standardized, EXCEPT cleanSheet_raw which uses original counts for the Poisson distribution.
     """
 
-    # Standardized priors centered around 0
-    saves_obs = pyro.sample("saves", dist.Normal(0, 1), obs=saves)
-    accuratePasses_obs = pyro.sample("accuratePasses", dist.Normal(0, 1), obs=accuratePasses)
-    ballRecovery_obs = pyro.sample("ballRecovery", dist.Normal(0, 1), obs=ballRecovery)
-
-    # goalsPrevented (continuous, standardized) depends on saves.
+    # Global Parameters (Outside Plate)
     alpha_gp = pyro.sample("alpha_gp", dist.Normal(0, 1))
     beta_saves_gp = pyro.sample("beta_saves_gp", dist.Normal(0, 1))
-    
-    gp_mu = alpha_gp + beta_saves_gp * saves_obs
     gp_sigma = pyro.sample("gp_sigma", dist.HalfNormal(1))
-    goalsPrevented_obs = pyro.sample("goalsPrevented", dist.Normal(gp_mu, gp_sigma), obs=goalsPrevented)
 
-    # cleanSheet (discrete counts) depends on goalsPrevented.
-    # We use a Poisson distribution. Since the rate (lambda) must be positive, 
-    # we use the log-link function.
     alpha_cs = pyro.sample("alpha_cs", dist.Normal(0, 1))
     beta_gp_cs = pyro.sample("beta_gp_cs", dist.Normal(0, 1))
-    
-    cs_log_rate = alpha_cs + beta_gp_cs * goalsPrevented_obs
-    cleanSheet_obs = pyro.sample("cleanSheet", dist.Poisson(torch.exp(cs_log_rate)), obs=cleanSheet_raw)
 
-    # Rating depends on all 5 nodes. 
     alpha_rating = pyro.sample("alpha_rating", dist.Normal(0, 1))
     w_saves = pyro.sample("w_saves", dist.Normal(0, 1))
     w_gp = pyro.sample("w_gp", dist.Normal(0, 1))
     w_cs = pyro.sample("w_cs", dist.Normal(0, 1))
     w_pass = pyro.sample("w_pass", dist.Normal(0, 1))
     w_recov = pyro.sample("w_recov", dist.Normal(0, 1))
-
-    rating_mu = (alpha_rating + 
-                 w_saves * saves_obs + 
-                 w_gp * goalsPrevented_obs + 
-                 w_cs * cleanSheet_obs + 
-                 w_pass * accuratePasses_obs + 
-                 w_recov * ballRecovery_obs)
-    
     rating_sigma = pyro.sample("rating_sigma", dist.HalfNormal(1))
-    rating_obs = pyro.sample("rating", dist.Normal(rating_mu, rating_sigma), obs=rating)
+
+    # Determine batch size
+    n_obs = 1
+    if saves is not None:
+        n_obs = saves.shape[0]
+    elif rating is not None:
+        n_obs = rating.shape[0]
+
+    # Use a plate to indicate conditionally independent observations
+    with pyro.plate("data", n_obs):
+        # Standardized priors centered around 0
+        saves_obs = pyro.sample("saves", dist.Normal(0, 1), obs=saves)
+        accuratePasses_obs = pyro.sample("accuratePasses", dist.Normal(0, 1), obs=accuratePasses)
+        ballRecovery_obs = pyro.sample("ballRecovery", dist.Normal(0, 1), obs=ballRecovery)
+
+        # goalsPrevented (continuous, standardized) depends on saves.
+        gp_mu = alpha_gp + beta_saves_gp * saves_obs
+        goalsPrevented_obs = pyro.sample("goalsPrevented", dist.Normal(gp_mu, gp_sigma), obs=goalsPrevented)
+
+        # cleanSheet (discrete counts) depends on goalsPrevented.
+        cs_log_rate = alpha_cs + beta_gp_cs * goalsPrevented_obs
+        cleanSheet_obs = pyro.sample("cleanSheet", dist.Poisson(torch.exp(cs_log_rate)), obs=cleanSheet_raw)
+
+        # Rating depends on all 5 nodes. 
+        rating_mu = (alpha_rating + 
+                     w_saves * saves_obs + 
+                     w_gp * goalsPrevented_obs + 
+                     w_cs * cleanSheet_obs + 
+                     w_pass * accuratePasses_obs + 
+                     w_recov * ballRecovery_obs)
+        
+        rating_obs = pyro.sample("rating", dist.Normal(rating_mu, rating_sigma), obs=rating)
 
     return rating_obs
 
