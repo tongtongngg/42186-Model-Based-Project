986534cbe2da5fc5e5f8c1ce7e34bfacb0b3ebb4
maltheaugust <s214631@dtu.dk>
2026-05-06 17:07:40 +0200

    model configs for evaluations

--- src/models/config.py
+++ src/models/config.py
@@ -1,6 +1,36 @@
+import pyro.poutine as poutine
 from src.models.attacker_model_basic import forward_model as attacker_model_fn
 from src.models.goalkeeper_model import goalkeeper_model as goalkeeper_model_fn
 from src.models.midfield_model import midfielder_model as midfielder_model_fn
+from src.models.combined_hierarchical_model import combined_hierarchical_model
+
+def combined_attacker_wrapper(**kwargs):
+    hidden = [
+        "mf_ohp", "mf_shots", "mf_fouled", "mf_xA", "mf_g", "mf_rating",
+        "gk_saves", "gk_pass", "gk_br", "gk_gp", "gk_cs", "gk_rating"
+    ]
+    return poutine.block(combined_hierarchical_model, hide=hidden)(attacker_data=kwargs)
+
+def combined_midfielder_wrapper(**kwargs):
+    hidden = [
+        "att_dw", "att_br", "att_kp", "att_ts", "att_xa", "att_sot", "att_g", "att_rating",
+        "gk_saves", "gk_pass", "gk_br", "gk_gp", "gk_cs", "gk_rating"
+    ]
+    return poutine.block(combined_hierarchical_model, hide=hidden)(midfielder_data=kwargs)
+
+def combined_goalkeeper_wrapper(**kwargs):
+    hidden = [
+        "att_dw", "att_br", "att_kp", "att_ts", "att_xa", "att_sot", "att_g", "att_rating",
+        "mf_ohp", "mf_shots", "mf_fouled", "mf_xA", "mf_g", "mf_rating"
+    ]
+    return poutine.block(combined_hierarchical_model, hide=hidden)(goalkeeper_data=kwargs)
+
+def combined_all_wrapper(attacker_data=None, midfielder_data=None, goalkeeper_data=None):
+    return combined_hierarchical_model(
+        attacker_data=attacker_data,
+        midfielder_data=midfielder_data,
+        goalkeeper_data=goalkeeper_data
+    )
 
 MODEL_CONFIGS = {
     "attacker": {
@@ -46,5 +76,80 @@ MODEL_CONFIGS = {
         },
         "target": "rating",
         "to_standardize": [] 
+    },
+    "combined_attacker": {
+        "model_fn": combined_attacker_wrapper,
+        "features": ['groundDuelsWon', 'ballRecovery', 'keyPasses', 'expectedAssists', 'totalShots', 'shotsOnTarget', 'goals'],
+        "pos_filter": 'F',
+        "feature_map": {
+            'groundDuelsWon': 'dw',
+            'ballRecovery': 'br',
+            'keyPasses': 'kp',
+            'expectedAssists': 'xa',
+            'totalShots': 'ts',
+            'shotsOnTarget': 'sot',
+            'goals': 'g_raw'
+        },
+        "target": "rating",
+        "to_standardize": ['groundDuelsWon', 'ballRecovery', 'keyPasses', 'expectedAssists', 'totalShots', 'shotsOnTarget']
+    },
+    "combined_midfielder": {
+        "model_fn": combined_midfielder_wrapper,
+        "features": ["accurateOppositionHalfPasses", "shotsFromOutsideTheBox", "wasFouled", "expectedAssists", "goals"],
+        "pos_filter": 'M',
+        "feature_map": {
+            'accurateOppositionHalfPasses': 'opp_half_passes',
+            'shotsFromOutsideTheBox': 'shots_outside',
+            'wasFouled': 'was_fouled',
+            'expectedAssists': 'xA',
+            'goals': 'goals'
+        },
+        "target": "rating",
+        "to_standardize": []
+    },
+    "combined_goalkeeper": {
+        "model_fn": combined_goalkeeper_wrapper,
+        "features": ['saves', 'accuratePasses', 'ballRecovery', 'goalsPrevented', 'cleanSheet'],
+        "pos_filter": 'G',
+        "feature_map": {
+            'saves': 'saves',
+            'accuratePasses': 'accuratePasses',
+            'ballRecovery': 'ballRecovery',
+            'goalsPrevented': 'goalsPrevented',
+            'cleanSheet': 'cleanSheet_raw'
+        },
+        "target": "rating",
+        "to_standardize": ['saves', 'accuratePasses', 'ballRecovery', 'goalsPrevented']
+    },
+    "combined_all": {
+        "model_fn": combined_all_wrapper,
+        "is_combined_all": True,
+        "positions": {
+            "attacker": {
+                "pos_filter": "F",
+                "features": ['groundDuelsWon', 'ballRecovery', 'keyPasses', 'expectedAssists', 'totalShots', 'shotsOnTarget', 'goals'],
+                "feature_map": {'groundDuelsWon': 'dw', 'ballRecovery': 'br', 'keyPasses': 'kp', 'expectedAssists': 'xa', 'totalShots': 'ts', 'shotsOnTarget': 'sot', 'goals': 'g_raw'},
+                "to_standardize": ['groundDuelsWon', 'ballRecovery', 'keyPasses', 'expectedAssists', 'totalShots', 'shotsOnTarget'],
+                "target_site": "att_rating",
+                "target": "rating"
+            },
+            "midfielder": {
+                "pos_filter": "M",
+                "features": ["accurateOppositionHalfPasses", "shotsFromOutsideTheBox", "wasFouled", "expectedAssists", "goals"],
+                "feature_map": {'accurateOppositionHalfPasses': 'opp_half_passes', 'shotsFromOutsideTheBox': 'shots_outside', 'wasFouled': 'was_fouled', 'expectedAssists': 'xA', 'goals': 'goals'},
+                "to_standardize": [],
+                "target_site": "mf_rating",
+                "target": "rating"
+            },
+            "goalkeeper": {
+                "pos_filter": "G",
+                "features": ['saves', 'accuratePasses', 'ballRecovery', 'goalsPrevented', 'cleanSheet'],
+                "feature_map": {'saves': 'saves', 'accuratePasses': 'accuratePasses', 'ballRecovery': 'ballRecovery', 'goalsPrevented': 'goalsPrevented', 'cleanSheet': 'cleanSheet_raw'},
+                "to_standardize": ['saves', 'accuratePasses', 'ballRecovery', 'goalsPrevented'],
+                "target_site": "gk_rating",
+                "target": "rating"
+            }
+        },
+        "target": "rating"
     }
 }
